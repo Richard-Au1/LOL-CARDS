dff1fbf216e8d15d6fb937530bdcd15b231f58ff
Ali Aldawoodi <124013342+Ali-Aldawoodi@users.noreply.github.com>
2023-12-05 19:20:44 -0700

    Added models folder along with corresponding files

---
+++ server/models/User.js
@@ -0,0 +1,21 @@
+const { Schema, model } = require('mongoose');
+
+const userSchema = new Schema ({
+    username: {
+        type: String,
+        require: true,
+    },
+    email: {
+        type: String,
+        required: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+});
+
+const User = model('User', userSchema);
+
+module.exports = User
+
